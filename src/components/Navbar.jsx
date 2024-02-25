7fc76555f8de2918812bc2ebcfc7762170be1212
Ankit <ankit.developer2004@gmail.com>
2024-02-25 22:42:07 +0530

    React UI Project Created

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import { FcSearch } from "react-icons/fc";
+import { IoFilter } from "react-icons/io5";
+import { BsToggle2Off } from "react-icons/bs";
+import { BsToggle2On } from "react-icons/bs";
+import logo from './layers.png';
+const Navbar = ({ bgColor, color, toggleButton,toggle }) => {
+  const bgcolor = color == "black"?"#cbc5c5":"black"
+  return (
+    <>
+      <div className="nav_bar"
+      style={{
+        backgroundColor:`${bgcolor}`
+      }}
+      >
+        <div
+          className="logo"
+          style={{
+            backgroundColor: `${bgColor}`,
+            color: `${color}`,
+          }}
+        >
+          <img src={logo}/>
+          React UI 
+          <div onClick={toggleButton} className='toggle'>
+            {toggle?(
+
+              <BsToggle2On />
+            ):(
+              <BsToggle2Off />
+            )}
+            {" "}
+          </div>
+        </div>
+        <div
+          className="search"
+          style={{
+            backgroundColor: `${bgColor}`,
+            color: `${color}`,
+          }}
+        >
+          <div className="left">
+            {" "}
+            <FcSearch /> Search here...
+          </div>
+          <div className="right">
+            <IoFilter /> Filters
+          </div>
+        </div>
+        <div
+          className="seller"
+          style={{
+            backgroundColor: `${bgColor}`,
+            color: `${color}`,
+          }}
+        >
+          Become a Seller
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar
